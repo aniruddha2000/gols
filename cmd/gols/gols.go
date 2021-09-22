e5557f2def621cb280070002dbf231fa0f30f2fb
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-09-22 21:19:09 +0530

    Update the code to support the urfave/cli interface

--- cmd/gols/gols.go
+++ cmd/gols/gols.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 func listDirectory(path string) {
@@ -20,20 +22,27 @@ func listDirectory(path string) {
 }
 
 func main() {
-	if len(os.Args) > 2 {
-		fmt.Println("3")
-		log.Fatal("Can't be more than 1 argument")
-	} else if len(os.Args) > 1 {
-		fmt.Println("1")
-		path := os.Args[1]
-		listDirectory(path)
-	} else {
-		fmt.Println("2")
-		path, err := os.Getwd()
-		if err != nil {
-			log.Fatal("Failed to get the directory name: %s", err)
-		}
-		fmt.Println(path)
-		listDirectory(path)
+	app := &cli.App{
+		Action: func(c *cli.Context) error {
+			if c.Args().Get(1) != "" {
+				log.Fatal("Can't be more than 1 argument")
+			} else if c.Args().Get(0) != "" {
+				path := c.Args().Get(0)
+				listDirectory(path)
+			} else {
+				path, err := os.Getwd()
+				if err != nil {
+					log.Fatal("Failed to get the directory name: %s", err)
+				}
+				listDirectory(path)
+			}
+			return nil
+		},
 	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
